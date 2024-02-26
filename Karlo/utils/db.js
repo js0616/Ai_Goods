72353811dab028b2e392e57eb45c8c610334ce77
이진솔 <136812080+js0616@users.noreply.github.com>
2024-02-26 22:00:14 +0900

    Update db.js

--- Karlo/utils/db.js
+++ Karlo/utils/db.js
@@ -2,11 +2,11 @@
 const mysql = require("mysql2");
 
 const dbConfig = {
-  host: "project-db-stu3.smhrd.com",
-  port: 3307,
-  user: "Insa4_JSA_hacksim_3",
-  password: "aishcool3",
-  database: "Insa4_JSA_hacksim_3",
+  host: "",
+  port: ,
+  user: "",
+  password: "",
+  database: "",
 };
 
 const pool = mysql.createPool(dbConfig);
